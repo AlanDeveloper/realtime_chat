3fb95af9277f78c9d7bb5036aa3adcfb142f258d
Alan Santos <alanssantos32@gmail.com>
2024-09-21 20:20:05 -0300

    Enviando dados para o mongodb

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/realtime_chat');
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.useDb('realtime_chat');
 
 const app = require('express')();
 
 const http = require('http').Server(app);
+
+const userRoute = require('./routes/userRoute');
+
+app.use('/', userRoute);
 
 http.listen(process.env.APP_PORT, function () {
     console.log(`Server is running`);
